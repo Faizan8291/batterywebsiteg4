8628beec29532b3312ba0163acabfeab25f88eda
Faizan8291 <123256763+Faizan8291@users.noreply.github.com>
2025-03-12 20:32:29 +0530

    Update scriptfaizan.js

--- docs/scriptfaizan.js
+++ docs/scriptfaizan.js
@@ -5,8 +5,7 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-const cors = require("cors");
-app.use(cors());
+
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
 
